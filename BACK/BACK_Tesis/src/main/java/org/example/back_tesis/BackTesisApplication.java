29410532a25ec143d2290ddb8982e50f75a64d03
Nicolas Nievas <nicoonievas7@gmail.com>
2024-09-18 15:34:06 -0300

    back

---
+++ BACK/BACK_Tesis/src/main/java/org/example/back_tesis/BackTesisApplication.java
@@ -0,0 +1,13 @@
+package org.example.back_tesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTesisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackTesisApplication.class, args);
+    }
+
+}
---
+++ BACK/BACK_Tesis/src/test/java/org/example/back_tesis/BackTesisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.back_tesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTesisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
