d7eff74bbabcb3463e5881e7e07922f7f6c436c1
Nicolas Nievas <nicoonievas7@gmail.com>
2024-09-23 17:24:44 -0300

    paleta de colores

--- FRONT/tailwind.config.ts
+++ FRONT/tailwind.config.ts
@@ -9,9 +9,15 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+        "gray-bg": "rgb(249 250 251)",
+        "gray-bg-light": "rgb(118 121 126)",
+        "gray-txt": "rgb(156 163 175)",
+        "black": "rgb(43 55 61)",
+        "black-btn": "rgb(23 26 37)",
+        "black-hover": "rgb(17 24 39)",
+        "blue": "rgb(59 130 246)",
+        "white": "rgb(255 253 249)"
+      }
     },
   },
   plugins: [],
