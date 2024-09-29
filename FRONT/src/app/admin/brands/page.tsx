be7877b0adf52ee3c1d79e15fe180d521d8c9040
Nicolas Nievas <nicoonievas7@gmail.com>
2024-09-29 17:41:39 -0300

    vista btn Create Category

--- FRONT/src/app/admin/brands/page.tsx
+++ FRONT/src/app/admin/brands/page.tsx
@@ -190,8 +190,7 @@ const BrandsPage: React.FC = () => {
             />
             <button 
               onClick={handleCreateCategory} 
-              className="2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-            >
+              className="ml-2 p-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
               Create Category
             </button>
           </div>
