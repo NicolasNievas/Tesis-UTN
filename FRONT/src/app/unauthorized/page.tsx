fe109685336b966cd146a114726e08a27be78afc
Nicolas Nievas <nicoonievas7@gmail.com>
2024-10-07 14:09:25 -0300

    UnauthorizedPage Styless

--- FRONT/src/app/unauthorized/page.tsx
+++ FRONT/src/app/unauthorized/page.tsx
@@ -1,20 +1,21 @@
 'use client'
 
 import Link from 'next/link';
+import Button from '@/components/Button';
 
 const UnauthorizedPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center p-8 bg-white rounded-lg shadow-md">
-        <h1 className="text-4xl font-bold text-red-600 mb-4">Acceso No Autorizado</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-gray-100 to-gray-300">
+      <div className="text-center p-8 bg-white rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-red-600 mb-6">Acceso No Autorizado</h1>
         <p className="text-gray-600 mb-6">
           Lo sentimos, no tienes permisos para acceder a esta sección.
         </p>
         <Link 
-          href="/" 
-          className="inline-block px-6 py-2 bg-black-500 text-white rounded hover:bg-blue-600 transition-colors"
-        >
-          Volver al Inicio
+          href="/">
+          <Button 
+            name="Volver al Inicio" 
+          />
         </Link>
       </div>
     </div>
