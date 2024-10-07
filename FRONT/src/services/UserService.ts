e4a40ba45115bc6c7272c6f9cb19510734e85d5c
Nicolas Nievas <nicoonievas7@gmail.com>
2024-10-07 13:09:46 -0300

    service const updateUserProfile

--- FRONT/src/services/UserService.ts
+++ FRONT/src/services/UserService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { IUserData } from '@/interfaces/data.interfaces';
+import { IUserData, UpdateUserRequest } from '@/interfaces/data.interfaces';
 import JWTService from '@/jwt/JwtService';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL_USERS;
@@ -15,6 +15,22 @@ export const getUserProfile = async (): Promise<IUserData> => {
         return response.data;
     } catch (error) {
         console.error("Error fetching user profile:", error);
+        throw error;
+    }
+};
+
+export const updateUserProfile = async (data: UpdateUserRequest): Promise<IUserData> => {
+    try {
+        const token = JWTService.getToken();
+        const response = await axios.put<IUserData>(`${API_URL}/update/profile`, data, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error updating user profile:", error);
         throw error;
     }
 };
